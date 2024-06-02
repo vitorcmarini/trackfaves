df50f232fd0a83de74aebcbc4a48b363417ae0c1
Vitor Marini <vitorcmarini@gmail.com>
2024-06-02 12:57:28 -0300

    first commit

---
+++ src/main/java/com/vcmarini/trackfaves/TrackfavesApplication.java
@@ -0,0 +1,13 @@
+package com.vcmarini.trackfaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackfavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackfavesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcmarini/trackfaves/TrackfavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcmarini.trackfaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackfavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
