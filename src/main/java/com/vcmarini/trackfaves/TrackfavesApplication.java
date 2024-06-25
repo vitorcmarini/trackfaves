c6fc2a2b2eab0d94683a06b2bc69573bd7cf79c4
vitor marini <vitorcmarini@hotmail.com>
2024-06-25 18:45:56 -0300

    add servico de leitura de api

--- src/main/java/com/vcmarini/trackfaves/TrackfavesApplication.java
+++ src/main/java/com/vcmarini/trackfaves/TrackfavesApplication.java
@@ -1,13 +1,22 @@
 package com.vcmarini.trackfaves;
 
+import com.vcmarini.trackfaves.service.APIConsumer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrackfavesApplication {
+public class TrackfavesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackfavesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumer = new APIConsumer();
+		var json = APIConsumer.catchData("https://api.jikan.moe/v4/manga");
+		System.out.println(json.toString());
 	}
 
 }
